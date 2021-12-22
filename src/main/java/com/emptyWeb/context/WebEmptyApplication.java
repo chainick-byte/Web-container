703fdd61b38f7812f8a2bb1799420c12bacb6d62
chainick-byte <igorrepyakh@gmail.com>
2021-12-22 17:19:52 +0000

    primer commit

---
+++ src/main/java/com/emptyWeb/context/WebEmptyApplication.java
@@ -0,0 +1,13 @@
+package com.emptyWeb.context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEmptyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEmptyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emptyWeb/context/WebEmptyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emptyWeb.context;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEmptyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
